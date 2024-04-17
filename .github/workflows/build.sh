488927a15637cc7613c491ae11016bb41b7d0135
Mimi <1119186082@qq.com>
2024-04-17 23:54:52 +0800

    Cleanup

--- .github/workflows/build.sh
+++
@@ -1,6 +0,0 @@
-ldd --version
-curl https://sh.rustup.rs -sSf | sh -s -- -y
-source "$HOME/.cargo/env"
-npm run build-release
-node -e "console.log(require('./index.node'))"
-node .github/workflows/deploy.js index.node hexo-optimize/bin/nodejs/$(node -e "console.log(require('./package.json').version);")/linux__x64.node
